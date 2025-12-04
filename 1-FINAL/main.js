c74b58c4ed2651eb886ed31ba6cc4e0a71334f60
Ellie Farbman <ellie.farbman@gmail.com>
2025-12-04 15:08:12 -0500

    final!

---
+++ 1-FINAL/main.js
@@ -0,0 +1,8 @@
+$(".speaker-button").click(
+    function(){
+        $(".r-speaker").toggleClass("on");
+        $(".r-loud").toggleClass("on");
+        $(".l-speaker").toggleClass("on");
+        $(".l-loud").toggleClass("on");
+    }
+);
